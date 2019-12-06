e5dcba5072e20ec124322d3d86cf809663c6f706
Sydney Benson <syzzle@Syzzle-Pro.local>
2019-12-06 17:52:44 -0500

    Initial commit

---
+++ src/main/java/com/example/sb_403/Sb403Application.java
@@ -0,0 +1,13 @@
+package com.example.sb_403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb403Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb_403/Sb403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb403ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
